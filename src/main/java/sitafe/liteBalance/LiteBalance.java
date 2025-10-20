b8d70308aea91123eb4466b9257ee685dac943e6
Sitafe <66419254+Sitafe@users.noreply.github.com>
2025-10-20 15:59:34 +0300

    Add files via upload

---
+++ src/main/java/sitafe/liteBalance/LiteBalance.java
@@ -0,0 +1,16 @@
+package sitafe.liteBalance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LiteBalance extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
